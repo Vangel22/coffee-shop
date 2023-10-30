19193207d4526b9e6d894afd88061b3961119c2e
Vangel22 <h.vangel22@gmail.com>
2023-10-30 13:32:12 +0100

    application base

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,18 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+dotenv.config();
+import "./db";
+import { routerCoffees } from "./routes/coffee-route";
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+export const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(routerCoffees);
+
+app.listen(process.env.PORT, () => {
+  return console.log(`Server started at port: [${process.env.PORT}]`);
 });
